12f46943afd977e4ebf33e6b9e95fbce702aad5d
sunyesle <sunyesle@gmail.com>
2024-05-07 00:37:41 +0900

    Initial commit

---
+++ src/main/java/com/sunyesle/atddmembership/AtddMembershipApplication.java
@@ -0,0 +1,13 @@
+package com.sunyesle.atddmembership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtddMembershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtddMembershipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunyesle/atddmembership/AtddMembershipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunyesle.atddmembership;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtddMembershipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
