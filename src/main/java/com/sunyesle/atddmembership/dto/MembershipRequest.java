45935e2e6e488c4be3bb3bc8a0bf3a69504a762a
sunyesle <sunyesle@gmail.com>
2024-05-10 23:55:18 +0900

    MembershipRequest 유효성 검사 추가

--- src/main/java/com/sunyesle/atddmembership/dto/MembershipRequest.java
+++ src/main/java/com/sunyesle/atddmembership/dto/MembershipRequest.java
@@ -1,11 +1,19 @@
 package com.sunyesle.atddmembership.dto;
 
 import com.sunyesle.atddmembership.enums.MembershipType;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 public class MembershipRequest {
+    @NotNull
     private final MembershipType membershipType;
+
+    @NotNull
+    @Min(0)
     private final Integer point;
 
     public MembershipRequest(MembershipType membershipType, Integer point) {
---
+++ src/test/java/com/sunyesle/atddmembership/MembershipRequestValidationTest.java
@@ -0,0 +1,70 @@
+package com.sunyesle.atddmembership;
+
+import com.sunyesle.atddmembership.dto.MembershipRequest;
+import com.sunyesle.atddmembership.enums.MembershipType;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.lang.reflect.Field;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MembershipRequestValidationTest {
+
+    @Autowired
+    private Validator validator;
+
+    @BeforeEach
+    void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    void MembershipRequest_유효성_검사_성공() {
+        MembershipRequest dto = getValidDto();
+
+        Set<ConstraintViolation<MembershipRequest>> constraintViolations = validator.validate(dto);
+
+        assertThat(constraintViolations).isEmpty();
+    }
+
+    static Stream<Arguments> provideFieldAndInvalidValue() {
+        return Stream.of(
+                Arguments.of("membershipType", null),
+                Arguments.of("point", null),
+                Arguments.of("point", -1)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideFieldAndInvalidValue")
+    void MembershipRequest_유효성_검사_실패(String fieldName, Object invalidValue) throws NoSuchFieldException, IllegalAccessException {
+        MembershipRequest dto = getValidDto();
+        Field field = MembershipRequest.class.getDeclaredField(fieldName);
+        field.setAccessible(true);
+        field.set(dto, invalidValue);
+
+        Set<ConstraintViolation<MembershipRequest>> violations = validator.validate(dto);
+
+        assertThat(violations.size()).isOne();
+        System.out.println(dto);
+        for (ConstraintViolation<MembershipRequest> violation : violations) {
+            System.out.println(violation.getMessage());
+        }
+    }
+
+    private MembershipRequest getValidDto() {
+        return new MembershipRequest(MembershipType.NAVER, 10000);
+    }
+}
