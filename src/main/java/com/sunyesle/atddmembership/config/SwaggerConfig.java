e3636353a446958f3a619992055e9ad67b0b3e4f
sunyesle <sunyesle@gmail.com>
2024-05-11 19:15:25 +0900

    Swagger 적용

---
+++ src/main/java/com/sunyesle/atddmembership/config/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.sunyesle.atddmembership.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("멤버십 적립 서비스 API"));
+    }
+}
