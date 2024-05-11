9b0fbf5aa71ee5fe4e4e8038db7a054ae196470f
sunyesle <sunyesle@gmail.com>
2024-05-11 17:42:55 +0900

    MembershipAccumulateRequest 유효성 검사 추가

--- src/main/java/com/sunyesle/atddmembership/dto/MembershipAccumulateRequest.java
+++ src/main/java/com/sunyesle/atddmembership/dto/MembershipAccumulateRequest.java
@@ -1,10 +1,16 @@
 package com.sunyesle.atddmembership.dto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 public class MembershipAccumulateRequest {
+    @NotNull
+    @Min(0)
     private final Integer amount;
 
     @JsonCreator
---
+++ src/test/java/com/sunyesle/atddmembership/MembershipAccumulateRequestValidationTest.java
@@ -0,0 +1,69 @@
+package com.sunyesle.atddmembership;
+
+import com.sunyesle.atddmembership.dto.MembershipAccumulateRequest;
+import com.sunyesle.atddmembership.enums.MembershipType;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.lang.reflect.Field;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MembershipAccumulateRequestValidationTest {
+
+    @Autowired
+    private Validator validator;
+
+    @BeforeEach
+    void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    void MembershipAccumulateRequest_유효성_검사_성공() {
+        MembershipAccumulateRequest dto = getValidDto();
+
+        Set<ConstraintViolation<MembershipAccumulateRequest>> constraintViolations = validator.validate(dto);
+
+        assertThat(constraintViolations).isEmpty();
+    }
+
+    static Stream<Arguments> provideFieldAndInvalidValue() {
+        return Stream.of(
+                Arguments.of("amount", null),
+                Arguments.of("amount", -1)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideFieldAndInvalidValue")
+    void MembershipAccumulateRequest_유효성_검사_실패(String fieldName, Object invalidValue) throws NoSuchFieldException, IllegalAccessException {
+        MembershipAccumulateRequest dto = getValidDto();
+        Field field = MembershipAccumulateRequest.class.getDeclaredField(fieldName);
+        field.setAccessible(true);
+        field.set(dto, invalidValue);
+
+        Set<ConstraintViolation<MembershipAccumulateRequest>> violations = validator.validate(dto);
+
+        assertThat(violations.size()).isOne();
+        System.out.println(dto);
+        for (ConstraintViolation<MembershipAccumulateRequest> violation : violations) {
+            System.out.println(violation.getMessage());
+        }
+    }
+
+    private MembershipAccumulateRequest getValidDto() {
+        return new MembershipAccumulateRequest(10000);
+    }
+}
