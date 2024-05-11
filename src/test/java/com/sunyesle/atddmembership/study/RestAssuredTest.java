7443d6ae9aa0b4a0755309513b8e9204088374f1
sunyesle <sunyesle@gmail.com>
2024-05-11 17:57:05 +0900

    RestAssuredTest 비활성화

--- src/test/java/com/sunyesle/atddmembership/study/RestAssuredTest.java
+++ src/test/java/com/sunyesle/atddmembership/study/RestAssuredTest.java
@@ -1,10 +1,12 @@
 package com.sunyesle.atddmembership.study;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
 import static io.restassured.RestAssured.given;
 
+@Disabled
 class RestAssuredTest {
 
     @Test
