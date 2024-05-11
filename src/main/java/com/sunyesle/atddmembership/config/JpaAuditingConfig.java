15de08b179344e721320ecda9f54b108096988cb
sunyesle <sunyesle@gmail.com>
2024-05-11 16:59:06 +0900

    @EnableJpaAuditing 설정 분리

---
+++ src/main/java/com/sunyesle/atddmembership/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.sunyesle.atddmembership.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
