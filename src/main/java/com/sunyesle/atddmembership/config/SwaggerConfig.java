4f166fa500cd242a7bd1f6aa71b576a2fcdedf44
sunyesle <sunyesle@gmail.com>
2024-05-27 23:06:38 +0900

    Swagger JWT 인증 설정

--- src/main/java/com/sunyesle/atddmembership/config/SwaggerConfig.java
+++ src/main/java/com/sunyesle/atddmembership/config/SwaggerConfig.java
@@ -1,17 +1,44 @@
 package com.sunyesle.atddmembership.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+//@SecurityScheme(
+//        name = "Authorization",
+//        type = SecuritySchemeType.HTTP,
+//        scheme = "bearer",
+//        bearerFormat = "JWT"
+//)
+//@OpenAPIDefinition(
+//        info = @Info(title = "멤버십 적립 서비스 API"),
+//        security = @SecurityRequirement(name = "Authorization")
+//)
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("Bearer Token");
+
         return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("Bearer Token", securityScheme)
+                )
+                .addSecurityItem(securityRequirement)
                 .info(new Info()
-                        .title("멤버십 적립 서비스 API"));
+                        .title("멤버십 적립 서비스 API")
+                );
     }
 }
